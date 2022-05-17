71d964022367fb57fb19d61dcc5471f481ffe6e9
Pietro Weg <pietro.sera@compasso.com.br>
2022-05-16 21:49:33 -0300

    feat: screenhot button created

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { ArrowLeft, Camera } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton"
+import ScreenshotButton from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -12,6 +14,7 @@ export function FeedbackContentStep({
   handleRestartFeedback
 }: FeedbackContentStepProps) {
   const feebackTypeInfo = feedbackTypes[feedbackType];
+  const [screenshot, setScreenshot] = useState<string | null>(null)
 
   return (
     <>
@@ -39,12 +42,10 @@ export function FeedbackContentStep({
         />
 
         <footer className="flex gap-2 mt-2">
-          <button
-            type="button"
-            className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-          >
-            <Camera className="w-6 h-6" />
-          </button>
+          <ScreenshotButton
+            screenshot={screenshot}
+            setScreenshot={setScreenshot}
+          />
 
           <button
             type="submit"
