c6904c2ca8a8996a54f5ae2afe62e5dcdecdf586
Pietro Weg <pietrows29@gmail.com>
2022-06-18 18:30:27 -0300

    feart: cors config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { routes } from './routes';
+import cors from 'cors'
 
 // iniciando aplicação
 const app = express();
 
+// controle de segurança no back-end (quais endereços podem consumir o back-end)
+app.use(cors());
 // express não entende json do body
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
 
 app.listen(3333, () => {
